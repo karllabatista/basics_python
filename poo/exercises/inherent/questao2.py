de4f10ffc9ddc1099615b9b0d309adfa7ad2b5f2
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-12-06 18:32:47 -0400

    melhorias na classe

--- poo/exercises/inherent/questao2.py
+++ poo/exercises/inherent/questao2.py
@@ -13,42 +13,88 @@ class Veiculo:
         self.__modelo = modelo
 
 
-    def get_informacoes(self):
+    def informacoes(self):
         return f"Marca do Veiculo is {self.__marca} e modelo is {self.__modelo}"
+
+    '''
+    @property a way to define getters and setters (pythonic)
+    '''
+    @property #getter
+    def marca(self):
+        return self.__marca
+    
+    @marca.setter #setter
+    def marca(self,marca):
+        self.__marca = marca
+
+    @property 
+    def modelo(self):
+        return self.__modelo
+
+    @modelo.setter
+    def modelo(self,modelo):
+        self.__modelo= modelo
+ 
 
 class Carro(Veiculo):
 
    
 
-    def __init__(self,marca,modelo,num_portas):
+    def __init__(self,marca,modelo,num_portas,tipo_combustivel):
        
         super().__init__(marca, modelo) #accessa metodo init da superclasse
         self.__num_portas =  num_portas
-        self.__tipo_combustivel ='gasosa'
+        self.__tipo_combustivel =tipo_combustivel
         
-    def get_informacoes(self):
+    def informacoes(self):
         """
         Reutiliza o método informacoes da classe base e adiciona detalhes específicos do carro.
         """
-        return f"{super().get_informacoes()}, Portas: {self.__num_portas}, Combustivel:{self.__tipo_combustivel}"
+        return f"{super().informacoes()}, Portas: {self.__num_portas}, Combustivel:{self.__tipo_combustivel}"
+    
+    @property
+    def num_portas():
+        return self.__num_portas
+    
+    @num_portas.setter
+    def num_portas(self,num_portas):
+        self.__num_portas = num_portas
 
+    @property
+    def tipo_combustivel():
+        return self.__tipo_combustivel
 
+    @tipo_combustivel.setter
+    def tipo_combustivel(self,tipo_combustivel):
+        self.__tipo_combustivel =  tipo_combustivel
 
 class Moto(Veiculo):
 
-    nums_cilindradas = 0
     
     def __init__(self,marca,modelo,nums_cilindradas):
 
         super().__init__(marca,modelo)
         self.__nums_cilindradas = nums_cilindradas
 
-    def get_informacoes(self):
-        return f"{super().get_informacoes()}, Portas: {self.__nums_cilindradas}"
+    @property
+    def nums_cilindradas():
+        return self.__nums_cilindradas
+    
+    @nums_cilindradas.setter
+    def nums_cilindradas(self,nums_cilindradas):
+        self.__nums_cilindradas =  nums_cilindradas
+    
+    def informacoes(self):
+        return f"{super().informacoes()}, Numero cilindradas: {self.__nums_cilindradas}"
 
 
-car = Carro("Wolkswagem","polo 2024",4)
-print(car.get_informacoes())
+car = Carro("Wolkswagem","polo 2024",4,"flex")
+print(car.informacoes())
 
 moto = Moto("Yamaha", "Fazer", 150)
-print(moto.get_informacoes())
+print(moto.informacoes())
+
+car.modelo="pol0 2025"
+car.num_portas = 6
+print(car.informacoes())
+
